eb939cea144f9022b702ee0924b97e2d85b1ca7f
Julianna Green & Josue <student@epicodus.com>
2015-09-09 14:48:47 -0700

    start and finish project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,11 @@
-var pigLatin = function(word) {
-  var vowelArray = ['a', 'e', 'i', 'o', 'u'];
-  var firstLetter = word.charAt(0);
-  var startsWithVowel = false;
-  var counter = 0;
-  var totalConsonants = 0;
-  var wordArray = word.split("")
-
-  for(var i = 0; i < vowelArray.length; ++i) {
-    if (firstLetter === vowelArray[i]) {
-      startsWithVowel = true;
-    }
-  }
-
-  if (startsWithVowel === true) {
-      return word.concat("ay");
-  }
-
-  else if (startsWithVowel === false) {
-    for(var j = 0; j < wordArray.length; ++j) {
-      counter = 0;
-      for(var i = 0; i < vowelArray.length; ++i) {
-
-        if (wordArray[j] === "q" && wordArray[j + 1] === "u") {
-          ++j;
-          totalConsonants = totalConsonants + 2;
-        } else if (wordArray[j] === vowelArray[i]) {
-          ++counter;
-        }
-      }
-
-      if (counter === 0) {
-        ++totalConsonants;
-      } else {
-        break;
-      }
-    }
-
-    var consonants = wordArray.slice(0, totalConsonants);
-    var vowels = wordArray.slice(totalConsonants, wordArray.length);
-    return vowels.concat(consonants).concat("ay").join("").replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-  }
-};
-
-var pigLatinSentence = function(sentence) {
-  var sentenceArray = [];
-  var splitSentence = sentence.split(" ");
-
-  for(var i = 0; i < splitSentence.length; ++i) {
-    sentenceArray.push(pigLatin(splitSentence[i]));
-  }
-
-  return sentenceArray.join(" ");
-
-};
+var palindrome = function(word) {
+  return word.toLowerCase().split("").reverse().join("") === word.toLowerCase();
+}
 
 $(function() {
-  $("form#pigLatin").submit(function(event) {
-    var sentence = $("input#input").val();
-    var result = pigLatinSentence(sentence);
+  $("form#palindrome").submit(function(event) {
+    var word = $("input#input").val();
+    var result = palindrome(word);
 
     $(".output").text(result);
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,20 +1,5 @@
-describe('pigLatin', function() {
-  it("adds ay to the end of a word that starts with a vowel", function() {
-    expect(pigLatin("orange")).to.equal("orangeay");
-  });
-
-  it("moves consecutive consonants to end of word", function() {
-    expect(pigLatin("striation")).to.equal("iationstray");
-  });
-
-  it("if word contains qu at beginning of word, qu is sent to end of word", function() {
-    expect(pigLatin("squeal")).to.equal("ealsquay");
-  });
-
-});
-
-describe('pigLatinSentence', function() {
-  it("applies piglatin to each word of a sentence", function() {
-    expect(pigLatinSentence("orange striation squeal")).to.equal("orangeay iationstray ealsquay");
+describe('palindrome', function() {
+  it("returns true if the word is the same reversed", function() {
+    expect(palindrome("TacoCat")).to.equal(true);
   });
 });
