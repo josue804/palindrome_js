088e2bf0fac5131a7389a0178ae1ba69f288f7a5
Julianna Green & Josue <student@epicodus.com>
2015-09-09 13:34:08 -0700

    finish code to correctly do pig latin on one word.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ var pigLatin = function(word) {
   var startsWithVowel = false;
   var counter = 0;
   var totalConsonants = 0;
-  var consonantsArray = [];
   var wordArray = word.split("")
 
   for(var i = 0; i < vowelArray.length; ++i) {
@@ -20,24 +19,36 @@ var pigLatin = function(word) {
   else if (startsWithVowel === false) {
     for(var j = 0; j < wordArray.length; ++j) {
       counter = 0;
-      for(var i = 0; i <vowelArray.length; ++i) {
-        if (wordArray[j] === vowelArray[i]) {
+      for(var i = 0; i < vowelArray.length; ++i) {
+
+        if (wordArray[j] === "q" && wordArray[j + 1] === "u") {
+          ++j;
+          totalConsonants = totalConsonants + 2;
+        } else if (wordArray[j] === vowelArray[i]) {
           ++counter;
         }
       }
+
       if (counter === 0) {
         consonantsArray.push(wordArray[j]);
         ++totalConsonants;
-      }
-
-      else {
+      } else {
         break;
       }
     }
 
-    consonantsArray.reverse();
     var consonants = wordArray.slice(0, totalConsonants);
     var vowels = wordArray.slice(totalConsonants, wordArray.length);
     return vowels.concat(consonants).concat("ay").join("");
   }
+};
+
+var pigLatinSentence = function(sentence) {
+  var sentenceArray = [];
+  var splitSentence = sentence.split(" ");
+
+  for(var i = 0; i < splitSentence.length; ++i) {
+    
+  }
+
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('pigLatin', function() {
   it("moves consecutive consonants to end of word", function() {
     expect(pigLatin("striation")).to.equal("iationstray");
   });
+
+  it("if word contains qu at beginning of word, qu is sent to end of word", function() {
+    expect(pigLatin("squeal")).to.equal("ealsquay");
+  });
 });
