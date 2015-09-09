ec8c9ad09c8e00a1370f0522aaf1126fdb576a09
Julianna Green & Josue <student@epicodus.com>
2015-09-09 11:02:57 -0700

    add function for converting words starting with consonants to piglatin

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ var pigLatin = function(word) {
   var vowelArray = ['a', 'e', 'i', 'o', 'u'];
   var firstLetter = word.charAt(0);
   var startsWithVowel = false;
+  var counter = 0;
+  var totalConsonants = 0;
+  var consonantsArray = [];
+  var wordArray = word.split("")
 
   for(var i = 0; i < vowelArray.length; ++i) {
     if (firstLetter === vowelArray[i]) {
@@ -10,6 +14,30 @@ var pigLatin = function(word) {
   }
 
   if (startsWithVowel === true) {
-    return word.concat("ay");
+      return word.concat("ay");
+  }
+
+  else if (startsWithVowel === false) {
+    for(var j = 0; j < wordArray.length; ++j) {
+      counter = 0;
+      for(var i = 0; i <vowelArray.length; ++i) {
+        if (wordArray[j] === vowelArray[i]) {
+          ++counter;
+        }
+      }
+      if (counter === 0) {
+        consonantsArray.push(wordArray[j]);
+        ++totalConsonants;
+      }
+
+      else {
+        break;
+      }
+    }
+
+    consonantsArray.reverse();
+    var consonants = wordArray.slice(0, totalConsonants);
+    var vowels = wordArray.slice(totalConsonants, wordArray.length);
+    return vowels.concat(consonants).concat("ay").join("");
   }
 };
